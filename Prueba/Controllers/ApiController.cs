faf8090772789e27996c3126108461bf21dacecd
khristopher <cparada@dataifx.com>
2020-07-19 17:22:27 -0400

    aprendi asp.net alv

---
+++ Prueba/Controllers/ApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Prueba.Models;
+
+namespace Prueba.Controllers
+{
+    
+    public class ApiController : Controller
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("api")]
+        [HttpGet("api/Index")]
+        [HttpGet("api/Index/{id?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("api/privacy")]
+        [Route("api/privacy/{id?}")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prueba/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Prueba.Models;
+
+namespace Prueba.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Home")]
+        [Route("Home/Index")]
+        [Route("Home/Index/{id?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
